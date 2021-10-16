96ea74cde44445d4d922bbe230575e4ae427d00c
Nathan Reidy <nathanjreidy@gmail.com>
2021-10-16 11:47:56 +1100

    Added more controller methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
+  @Get()
   findAll(): string {
     return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
   }
 }
