44b8a2b4f2405e8edaa0f6c24589a03781587898
Nathan Reidy <nathanjreidy@gmail.com>
2021-10-19 17:28:52 +1100

    Handling malicious request data

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -32,6 +32,7 @@ export class CoffeesService {
 
   create(createCoffeeDto: any) {
     this.coffees.push(createCoffeeDto);
+    return createCoffeeDto;
   }
 
   update(id: string, updateCoffeeDto: any) {
