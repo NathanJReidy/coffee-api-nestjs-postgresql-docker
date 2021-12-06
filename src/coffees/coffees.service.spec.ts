c5a492d9782da76c67b1e64653d0c3f4582dc949
Nathan Reidy <nathanjreidy@gmail.com>
2021-12-06 20:11:36 +1100

    getting started with unit tests

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
+import coffeesConfig from './config/coffees.config';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        { provide: Connection, useValue: {} },
+        { provide: getRepositoryToken(Flavor), useValue: {} },
+        { provide: getRepositoryToken(Coffee), useValue: {} },
+        { provide: COFFEE_BRANDS, useValue: {} },
+        { provide: coffeesConfig.KEY, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -9,7 +9,7 @@ import {
 import { ConfigService, ConfigType } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
-import { Event } from 'src/events/entities/event.entity';
+import { Event } from '../events/entities/event.entity';
 import { Connection, Repository } from 'typeorm';
 import { COFFEE_BRANDS } from './coffees.constants';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
