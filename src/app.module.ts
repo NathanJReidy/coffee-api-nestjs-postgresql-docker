cca37204510906bbdbfe6e3d439c3c0e4e4bc714
Nathan Reidy <nathanjreidy@gmail.com>
2021-12-08 10:08:17 +1100

    introducing the Mongoose model

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
